c4249395ef13e2ef1400adafeef7be40b2c5c25e
Arne Imhof <arne.imhof@oasissoftware.de>
2023-11-14 07:39:29 +0100

    Sync mit deployter security group

--- sg_main.tf
+++ sg_main.tf
@@ -73,14 +73,14 @@ resource "aws_security_group" "LoadBalancerSecurityGroup" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["${var.cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "TLS from VPC"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["${var.cidr}"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
